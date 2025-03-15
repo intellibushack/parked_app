1cc9ab76fbd66963ee163a25ae5cf09d032ec79a
kiano-serha <kian.oconnor.serha@gmail.com>
2025-03-15 16:54:21 -0500

    add classes of vehicles

---
+++ database/migrations/2025_03_15_214649_create_classes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('min-height');
+            $table->integer('max-height');
+            $table->integer('min-length');
+            $table->integer('max-length');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classes');
+    }
+};
