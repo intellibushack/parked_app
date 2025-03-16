48e93e081baedad7986b50c7a1a9bbb271f90f28
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2025-03-16 06:44:54 -0500

    made changes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,7 +14,7 @@
                 <div class="btn-list">
 
                   @if(Auth::check())
-                  <a href="{{ route('register')}}" class="btn btn-primary">
+                  {{-- <a href="" onclick="handleLogout()" class="btn btn-primary"> --}}
                     <!-- Download SVG icon from http://tabler-icons.io/i/brand-github -->
                     {{-- <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                         viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
@@ -23,8 +23,13 @@
                         <path
                             d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
                     </svg> --}}
-                   Logout
-                </a>
+                   {{-- Logout --}}
+                {{-- </a> --}}
+                <form action="{{route('logout')}}" method="post">
+                  @csrf
+                  <button class="btn btn-primary" type="submit">Logout</button>
+                  {{-- <span class="fs-7">Log Out</span> --}}
+                </form>
                   @else
                   <a href="{{ route('register')}}" class="btn btn-primary">
                     <!-- Download SVG icon from http://tabler-icons.io/i/brand-github -->
@@ -662,3 +667,5 @@
       </div> --}}
     </div>
 </header>
+
+
